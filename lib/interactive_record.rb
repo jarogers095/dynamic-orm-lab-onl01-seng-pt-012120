3f04bf1189c8d78a5cf3847c9e724f72882d64aa
James Rogers <jarogers095@gmail.com>
2020-03-27 07:48:33 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,18 +2,19 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  def self.table_name
+  def self.table_name()
     self.to_s.downcase.pluralize
   end
   
-  def self.column_names(db)
+  def self.column_names()
     sql = "PRAGMA table_info(?);"
-    
     table_info = DB[:conn].execute(sql, table_name)
     column_names = []
+    
     table_info.each do |row|
       column_names << row["name"]
     end
+    
     column_names.compact
   end
 end
