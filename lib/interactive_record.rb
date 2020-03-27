ef361701e1f9f452ec7a30fff895ec9e0e2cf522
James Rogers <jarogers095@gmail.com>
2020-03-27 09:00:06 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -41,7 +41,7 @@ class InteractiveRecord
   end
   
   def save()
-    sql = "INSERT INTO students (?) VALUES (?);"
+    sql = "INSERT INTO students(?) VALUES(?);"
     DB[:conn].execute(sql, col_names_for_insert, values_for_insert)
   end
 end
